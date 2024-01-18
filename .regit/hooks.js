549417bd4efe3f55dcbc6392b3f0cf9e45a56b00
Alexandre Guidet <a.guidet@we-are-mea.com>
2024-01-19 11:59:48 +1300

    updated

--- .regit/hooks.js
+++ .regit/hooks.js
@@ -1,4 +1,9 @@
 import { execSync } from 'child_process'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const rootFolder = `${__dirname}/../`
 
