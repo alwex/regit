8936d5c8fe70d5e13ca5034e360ebe6391559436
Alexandre Guidet <a.guidet@we-are-mea.com>
2024-02-03 15:20:41 +1300

    added hooks

--- .regit/hooks.js
+++ .regit/hooks.js
@@ -45,12 +45,18 @@ export default {
     postFeatureStart: async (id) => {
         // do something clever after feature start
     },
-    preReleaseFinish: async (id) => {
+    preReleaseStart: async (version) => {
+        // do something clever before release start
+    },
+    preReleaseStart: async (version) => {
+        // do something clever before release start
+    },
+    preReleaseFinish: async (version) => {
         // do something clever before release finish
         // generateChangelog()
-        setPackageVersion(id)
+        setPackageVersion(version)
     },
-    postReleaseFinish: async (id) => {
+    postReleaseFinish: async (version) => {
         // do something clever after release finish
     },
 }
